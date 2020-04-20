17dd38262b8fdd3121dafa52d0f9f4c3d2a50655
fuston05 <fuston05@yahoo.com>
2020-04-20 19:24:17 -0400

    added dotenv, and port to graphql server in 'opts' obj for the server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const {GraphQLServer } = require("graphql-yoga")
 const { prisma } = require("../prisma/src/generated/prisma.client")
 const Query = require("./resolvers/Query")
 const Mutation = require("./resolvers/mutation")
 // const Project = require("./resolvers/Projects")
 
+const opts= {
+  port: process.env.PORT
+}
 
 const resolvers = {
     Query,
@@ -14,10 +19,11 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: "./src/schema.graphql",
     resolvers,
+    opts,
     context: request => ({
         ...request,
         prisma
     })
 })
 
-server.start(() => console.log(`Server is up`))
+server.start(() => console.log(`Server is on port: ${opts.port}`))
