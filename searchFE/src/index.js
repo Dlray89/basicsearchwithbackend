caaa71a84f1a1015ee28794b71662bf966219d7c
DAP <dlrayjr89@gmail.com>
2020-04-21 14:28:29 +0000

    add exchanges to index.js

--- searchFE/src/index.js
+++ searchFE/src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createClient, Provider} from 'urql';
+import {Client, Provider, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from "@urql/exchange-graphcache"
 
 
-const client= createClient({
-  url: `https://4000-c7641b3a-5cc2-4046-b7ef-eb0a379f6695.ws-us02.gitpod.io/`
-  //** add this code back for auth */
-  // fetchOptions: () => {
-  //   const token= getToken();
-  //   return {
-  //     headers: {authorization: token ? `Bearer ${token}` : ''}
-  //   }
-  // }
+const cache = cacheExchange({})
+
+const client=  new Client({
+  url: `https://4000-c7641b3a-5cc2-4046-b7ef-eb0a379f6695.ws-us02.gitpod.io/`,
+  exchanges: [dedupExchange, cache, fetchExchange]
+
+
 });// end client
 
 ReactDOM.render(
